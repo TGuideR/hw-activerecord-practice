6e98d375f35b9a215a8f1ffd2822cd0e26160c49
TGuideR <thanakorng2147@gmail.com>
2024-01-14 04:03:38 +0000

    Finished

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -9,6 +9,7 @@ ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => 'cus
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 class Customer < ActiveRecord::Base
+
   def to_s
     "  [#{id}] #{first} #{last}, <#{email}>, #{birthdate.strftime('%Y-%m-%d')}"
   end
@@ -19,9 +20,65 @@ class Customer < ActiveRecord::Base
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    Customer.where(:first => "Candice")
   end
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE ?", '%@%')
   end
   # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE ?", '%.org')
+  end
+
+
+  def self.with_invalid_email
+    Customer.where("email NOT LIKE ?", '%@%')
+  end
+
+  def self.with_blank_email
+    Customer.where("email IS NULL")
+  end
+
+  def self.born_before_1980
+    Customer.where('birthdate < ?', '1980-#-#')
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email LIKE ? AND birthdate < ?", '%@%', '1980-#-#')
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE ?", 'B%').order('birthdate ASC')
+  end
+
+  def self.twenty_youngest
+    Customer.order('birthdate DESC').limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    gussie = Customer.find_by(first: "Gussie")
+    gussie.birthdate = '2004-02-08'
+    gussie.save
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    invalid = Customer.where("email NOT LIKE ?", "%@%")
+    invalid.each do |rec|
+      rec.update(email: '')
+    end
+  end
+
+  def self.delete_meggie_herman
+    meggie = Customer.find_by(first: "Meggie", last: "Herman")
+    meggie.destroy
+  end
+
+  def self.delete_everyone_born_before_1978
+    everyone = Customer.where('birthdate < ?', "1978-#-#")
+    everyone.each do |rec|
+      rec.destroy
+    end
+  end
 end
